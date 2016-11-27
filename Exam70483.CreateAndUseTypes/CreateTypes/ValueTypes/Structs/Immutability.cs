1693c17b548b90c0ebf130a91e0a504a2d3a900c
Dan <=>
2016-11-27 12:53:19 +0000

    immutable value types

---
+++ Exam70483.CreateAndUseTypes/CreateTypes/ValueTypes/Structs/Immutability.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exam70483.CreateAndUseTypes.CreateTypes.ValueTypes.Structs
+{
+    internal class Immutability
+    {
+        public void Method()
+        {
+            // value types are immutable
+            var date = new DateTime(2016, 11, 27);
+
+            // even though DateTime exposes methods such as AddDays,
+            // they do not mutate the original variable, rather a new updated
+            // DateTime instance is returned
+            date.AddDays(1);
+
+            // ^ date is still equal to 27/11/16 as the returned DateTime wasn't assigned
+
+            // string is also an immutable struct
+            const string name = " bobby conn ";
+            name.Trim();
+            
+            // ^ name still contains whitespace
+        }
+    }
+}
