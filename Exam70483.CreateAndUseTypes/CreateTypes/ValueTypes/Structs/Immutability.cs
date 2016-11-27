2f3ae065d5d4035fcfa7da03a22f4f0cc33499b9
Dan <=>
2016-11-27 12:55:52 +0000

    immutable value types

--- Exam70483.CreateAndUseTypes/CreateTypes/ValueTypes/Structs/Immutability.cs
+++ Exam70483.CreateAndUseTypes/CreateTypes/ValueTypes/Structs/Immutability.cs
@@ -16,7 +16,10 @@ namespace Exam70483.CreateAndUseTypes.CreateTypes.ValueTypes.Structs
 
             // ^ date is still equal to 27/11/16 as the returned DateTime wasn't assigned
 
-            // string is also an immutable struct
+            // string is a reference type, it is a reference that points to a sequence
+            // of characters.
+            // however whilst they are reference types, they are immutable and behave
+            // in many ways like a value type
             const string name = " bobby conn ";
             name.Trim();
             
