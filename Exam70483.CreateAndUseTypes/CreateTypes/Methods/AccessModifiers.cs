67876989d9006e6df76a4b6241da48c724c87542
Dan <=>
2016-11-27 13:43:21 +0000

    access modifiers

---
+++ Exam70483.CreateAndUseTypes/CreateTypes/Methods/AccessModifiers.cs
@@ -0,0 +1,20 @@
+﻿namespace Exam70483.CreateAndUseTypes.CreateTypes.Methods
+{
+    public class AccessModifiers
+    {
+        public void Hello()
+        {
+            // public - available everywhere
+        }
+
+        private void Shh()
+        {
+            // private - only available to code inside same class
+        }
+
+        internal void Hey()
+        {
+            // internal - only available to code in the same project
+        }
+    }
+}
