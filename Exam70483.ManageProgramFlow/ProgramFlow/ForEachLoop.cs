6d54e32416dbf8d3b40beca8247fec70c99e6e08
Dan <=>
2016-12-04 16:07:42 +0000

    flow control

---
+++ Exam70483.ManageProgramFlow/ProgramFlow/ForEachLoop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Exam70483.ManageProgramFlow.ProgramFlow
+{
+    internal class ForEachLoop
+    {
+        public void Method()
+        {
+            var things = new List<string>()
+            {
+                "cat",
+                "sat",
+                "mat"
+            };
+
+            foreach (var thing in things)
+            {
+                if(thing.Equals("mat")) continue;   // don't even bother with the following code, continue to next iteration
+                if (thing.Equals("sat")) break;     // jump out of the loop all together and stop iterating
+            }
+        }
+    }
+}
