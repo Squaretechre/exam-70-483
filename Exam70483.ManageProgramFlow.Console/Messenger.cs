fca6a34989e5597c6a0a7879e33053f04ce6607e
Dan <=>
2016-12-14 22:14:16 +0000

    stop / stop threads

---
+++ Exam70483.ManageProgramFlow.Console/Messenger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Exam70483.ManageProgramFlow.ConsoleApp
+{
+    internal class Messenger
+    {
+        private readonly string _message;
+
+        public Messenger(string message)
+        {
+            _message = message;
+        }
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine("[{0}] {1}", Thread.CurrentThread.ManagedThreadId, _message);
+        }
+    }
+}
