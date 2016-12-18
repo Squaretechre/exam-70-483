cdcf3fa635c8b0153fbe1ae5983abe16720e908c
Dan <=>
2016-12-18 22:32:36 +0000

    threading summary

---
+++ Exam70483.ManageProgramFlow.Console/ThreadingSummary.cs
@@ -0,0 +1,19 @@
+﻿namespace Exam70483.ManageProgramFlow.ConsoleApp
+{
+    internal class ThreadingSummary
+    {
+        // Most resources are [not] meant to be accessed concurrently
+        // - improper access yields race conditions and incorrect results
+        //      - very difficult to detect, debug and fix
+        //
+        // Common solutions
+        // - atomic updates of machine-word-sized values
+        // - data partitioning
+        // - wait-based synchronization (monitor, mutex, others)
+        //
+        // Beware
+        // - critical sections
+        // - race conditions
+        // - deadlock
+    }
+}
